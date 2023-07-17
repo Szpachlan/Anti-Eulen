bae43b229bb712bdb345655d0c01b7211da9034e
Ciastekbatak <73557171+CiastekbatakPro@users.noreply.github.com>
2023-07-17 12:59:34 +0200

    Delete old files

--- config.lua
+++
@@ -1,2 +0,0 @@
-fiveguardbans = false -- Change to true if you want to use ban system from fiveguard
-fiveguardname = "yourname" -- Change to name of your fiveguard resource
--- fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'bodacious'
-game 'gta5'
-
-client_script 'client.lua'
-server_script 'config.lua'
-server_script 'server.lua'
